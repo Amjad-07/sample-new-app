46e729bcb6b62bd970fb918ae3e62ce1a3faecf0
Amjad-07 <146514436+Amjad-07@users.noreply.github.com>
2026-01-30 15:48:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Cloud Run + Jenkins + Artifact Registry!"
+    return "Hello from Cloud Run + Jenkins!"
 
-if __name__ == "__main__":
+def start_server():
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    # Only start server if RUN_SERVER=true
+    if os.environ.get("RUN_SERVER", "false").lower() == "true":
+        start_server()
